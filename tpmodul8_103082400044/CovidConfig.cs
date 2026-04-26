d96d5dfdbbb21dff38d4e4c0bd04fba951f8eaa1
ditt-git <firdaus.radit2020@gmail.com>
2026-04-26 18:55:01 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103082400044/CovidConfig.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpmodul8_103082400044
+{
+    public class ConfigData
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public ConfigData() { }
+
+        public ConfigData(string satuan_suhu, int batas_hari_deman, string pesan_ditolak, string pesan_diterima)
+        {
+            this.satuan_suhu = satuan_suhu;
+            this.batas_hari_deman = batas_hari_deman;
+            this.pesan_ditolak = pesan_ditolak;
+            this.pesan_diterima = pesan_diterima;
+        }
+    }
+
+    public class CovidConfig
+    {
+        public ConfigData config;
+        private const string filePath = "covid_config.json";
+
+        public CovidConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+        private void ReadConfigFile()
+        {
+            string configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<ConfigData>(configJsonData);
+        }
+
+        private void SetDefault()
+        {
+            config = new ConfigData(
+                "celcius",
+                14,
+                "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+            );
+        }
+
+        private void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public void UbahSatuan()
+        {
+            if (config.satuan_suhu == "celcius")
+            {
+                config.satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                config.satuan_suhu = "celcius";
+            }
+            WriteConfigFile();
+        }
+    }
+}
--- tpmodul8_103082400044/Program.cs
+++ tpmodul8_103082400044/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul8_103082400044;
+
+namespace tpmodul8_103082400036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CovidConfig covidConfig = new CovidConfig();
+
+
+            JalankanAplikasi(covidConfig);
+
+
+            Console.WriteLine("\n--- Memanggil Method UbahSatuan ---");
+            covidConfig.UbahSatuan();
+
+
+            JalankanAplikasi(covidConfig);
+        }
+
+        static void JalankanAplikasi(CovidConfig covidConfig)
+        {
+            Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {covidConfig.config.satuan_suhu}");
+            double suhu = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+            int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+            bool suhuValid = false;
+
+            if (covidConfig.config.satuan_suhu == "celcius")
+            {
+                if (suhu >= 36.5 && suhu <= 37.5)
+                {
+                    suhuValid = true;
+                }
+            }
+            else if (covidConfig.config.satuan_suhu == "fahrenheit")
+            {
+                if (suhu >= 97.7 && suhu <= 99.5)
+                {
+                    suhuValid = true;
+                }
+            }
+
+            if (suhuValid && hariDemam < covidConfig.config.batas_hari_deman)
+            {
+                Console.WriteLine(covidConfig.config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(covidConfig.config.pesan_ditolak);
+            }
+        }
+    }
+}
